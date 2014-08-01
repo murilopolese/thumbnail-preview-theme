4054d375b3a50cc31fc2450625d8c51f736f8e3e
Murilo Polese <murilopolese@gmail.com>
2014-08-02 00:04:03 +0200

    Pages and pages order

--- index.php
+++ index.php
@@ -38,104 +38,129 @@
             </section>
         </header>
 
+        <main>
 
-        <?php
-        $posts_per_page = 4;
-        $args = array(
-            'post_type'         => 'post',
-            'posts_per_page'    => $posts_per_page
-        );
-        $posts = new WP_Query( $args );
-        ?>
+            <div class="page">
+                <?php
+                $args = array(
+                    'post_type'         => 'page',
+                    'posts_per_page'    => 1,
+                    'orderby'           => 'menu_order',
+                    'order'             => 'ASC'
+                );
+                $posts = new WP_Query( $args );
+                ?>
+                <?php while( $posts->have_posts() ) : ?>
+                    <?php $posts->the_post(); ?>
+                    <section>
+                        <article>
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_content(); ?>
+                        </article>
+                    </section>
+                <?php endwhile; ?>
+            </div>
 
-        <?php for( $i = 1; $i <= $posts->max_num_pages; $i++ ) : ?>
             <?php
+            $posts_per_page = 4;
             $args = array(
                 'post_type'         => 'post',
-                'posts_per_page'    => $posts_per_page,
-                'paged'             => $i
+                'posts_per_page'    => $posts_per_page
             );
             $posts = new WP_Query( $args );
             ?>
 
-            <div class="thumbnail-row">
-                <section>
-                    <?php while( $posts->have_posts() ) : ?>
-                        <?php $posts->the_post(); ?>
-                        <?php
-                            // Skip posts without thumbnail
-                            if( !has_post_thumbnail() ) continue;
-                            // Get large thumbnail url
-                            $thumbnail = reset(
-                                wp_get_attachment_image_src(
-                                    get_post_thumbnail_id(),
-                                    'thumbnail'
-                                )
-                            );
-                        ?>
-                        <article>
-                            <a href="#post-<?php the_ID(); ?>">
-                                <img src="<?php echo $thumbnail; ?>" width="100%" />
-                            </a>
-                        </article>
-                    <?php endwhile; ?>
-                </section>
-            </div>
+            <?php for( $i = 1; $i <= $posts->max_num_pages; $i++ ) : ?>
+                <?php
+                $args = array(
+                    'post_type'         => 'post',
+                    'posts_per_page'    => $posts_per_page,
+                    'paged'             => $i
+                );
+                $posts = new WP_Query( $args );
+                ?>
 
-            <div class="preview-full-width">
-                <div class="preview-row">
-                    <?php while( $posts->have_posts() ) : ?>
-                        <?php $posts->the_post(); ?>
-                        <?php
-                        	// Skip posts without thumbnail
-                        	if( !has_post_thumbnail() ) continue;
-                        	// Get large thumbnail url
-                        	$large_image_url = reset(
-                                wp_get_attachment_image_src(
-                            		get_post_thumbnail_id(),
-                            		'large'
-                            	)
-                            );
-                            $post_custom = get_post_custom();
-                            if( empty( $post_custom[ 'post_link' ] ) ) {
-                                $post_custom[ 'post_link' ] = array( '#' );
-                            }
-                            if( empty( $post_custom[ 'post_link_text' ] ) ) {
-                                $post_custom[ 'post_link_text' ] = array( 'View more' );
-                            }
-                            $post_custom[ 'post_link' ] = reset( $post_custom[ 'post_link' ] );
-                            $post_custom[ 'post_link_text' ] = reset( $post_custom[ 'post_link_text' ] );
-                        ?>
-                        <section id="post-<?php the_ID(); ?>">
+                <div class="thumbnail-row">
+                    <section>
+                        <?php while( $posts->have_posts() ) : ?>
+                            <?php $posts->the_post(); ?>
+                            <?php
+                                // Skip posts without thumbnail
+                                if( !has_post_thumbnail() ) continue;
+                                // Get large thumbnail url
+                                $thumbnail = reset(
+                                    wp_get_attachment_image_src(
+                                        get_post_thumbnail_id(),
+                                        'thumbnail'
+                                    )
+                                );
+                            ?>
                             <article>
-                                <figure>
-                                    <img src="<?php echo $large_image_url; ?>"
-                                        width="100%" />
-                                </figure>
-                                <aside>
-                                    <h1><?php the_title(); ?></h1>
-                                    <?php the_content(); ?>
-                                    <?php if( $post_custom[ 'post_link' ] != '#' ) : ?>
-                                        <p>
-                                            <a href="<?php echo $post_custom[ 'post_link' ]; ?>">
-                                                <?php echo $post_custom[ 'post_link_text' ]; ?>
-                                            </a>
-                                        </p>
-                                    <?php endif; ?>
-                                </aside>
+                                <a href="#post-<?php the_ID(); ?>">
+                                    <img src="<?php echo $thumbnail; ?>" width="100%" />
+                                </a>
                             </article>
-                        </section>
-                    <?php endwhile; ?>
+                        <?php endwhile; ?>
+                    </section>
                 </div>
-            </div>
 
-        <?php endfor; ?>
+                <div class="preview-full-width">
+                    <div class="preview-row">
+                        <?php while( $posts->have_posts() ) : ?>
+                            <?php $posts->the_post(); ?>
+                            <?php
+                            	// Skip posts without thumbnail
+                            	if( !has_post_thumbnail() ) continue;
+                            	// Get large thumbnail url
+                            	$large_image_url = reset(
+                                    wp_get_attachment_image_src(
+                                		get_post_thumbnail_id(),
+                                		'large'
+                                	)
+                                );
+                                $post_custom = get_post_custom();
+                                if( empty( $post_custom[ 'post_link' ] ) ) {
+                                    $post_custom[ 'post_link' ] = array( '#' );
+                                }
+                                if( empty( $post_custom[ 'post_link_text' ] ) ) {
+                                    $post_custom[ 'post_link_text' ] = array( 'View more' );
+                                }
+                                $post_custom[ 'post_link' ] = reset( $post_custom[ 'post_link' ] );
+                                $post_custom[ 'post_link_text' ] = reset( $post_custom[ 'post_link_text' ] );
+                            ?>
+                            <section id="post-<?php the_ID(); ?>">
+                                <article>
+                                    <figure>
+                                        <img src="<?php echo $large_image_url; ?>"
+                                            width="100%" />
+                                    </figure>
+                                    <aside>
+                                        <h1><?php the_title(); ?></h1>
+                                        <?php the_content(); ?>
+                                        <?php if( $post_custom[ 'post_link' ] != '#' ) : ?>
+                                            <p>
+                                                <a href="<?php echo $post_custom[ 'post_link' ]; ?>">
+                                                    <?php echo $post_custom[ 'post_link_text' ]; ?>
+                                                </a>
+                                            </p>
+                                        <?php endif; ?>
+                                    </aside>
+                                </article>
+                            </section>
+                        <?php endwhile; ?>
+                    </div>
+                </div>
+
+            <?php endfor; ?>
+        </main>
 
         <footer>
             <?php
             $args = array(
                 'post_type'         => 'page',
-                'posts_per_page'    => -1
+                'orderby'           => 'menu_order',
+                'order'             => 'ASC',
+                'offset'            => 1
             );
             $posts = new WP_Query( $args );
             ?>
@@ -148,7 +173,6 @@
                     </article>
                 </section>
             <?php endwhile; ?>
-
         </footer>
 
         <script src="<?php bloginfo( 'template_directory' ); ?>/js/vendor/jquery.min.js"></script>
